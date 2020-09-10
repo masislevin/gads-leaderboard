ac81c321b8fb3eaaf9b54d548224ea57466ed14e
Levin Masis <masislevin@gmail.com>
2020-09-10 14:43:13 +0300

    gads fragments inflate layouts commit

--- app/src/main/java/com/levin/gads/gads/leaderboard/SubmissionSuccessFragment.java
+++ app/src/main/java/com/levin/gads/gads/leaderboard/SubmissionSuccessFragment.java
@@ -1,6 +1,21 @@
 package com.levin.gads.gads.leaderboard;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
 public class SubmissionSuccessFragment extends DialogFragment {
+    public SubmissionSuccessFragment() { }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_submission_failed, container);
+    }
 }
